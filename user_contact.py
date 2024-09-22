04745fa8b38753209f952d958dd6cd0332e81363
GStorm888 <denchikevstr@icloud.com>
2024-09-22 12:17:21 +0300

    Создал представление для контакта пользователя
    Пользователь  обладает именем, номером телефона и опциональным описанием

---
+++ user_contact.py
@@ -0,0 +1,13 @@
+class UserContact:
+    def __init__(self,
+                name: str,
+                phone_number: str,
+                description:str = "",
+                ):
+        self.name = name
+        self.phone_numder = phone_number
+        self.description = description
+
+    def __str__(self):
+        return f"Имя: {self.name}, номер телефона: {self.phone_numder}"
+    
